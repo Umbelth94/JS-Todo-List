83729eeeb04ccace08f554d4c066eb0a66b8cb34
Travis Umbel <Umbelth94@gmail.com>
2023-12-29 12:31:31 -0600

    Add remaining workday hours, get rid of time-sensitive classes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+//TODO: Populate all time blocks
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
